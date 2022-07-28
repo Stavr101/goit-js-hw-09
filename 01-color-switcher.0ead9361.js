42d8a1b2262bf3f76d02f5572e5e66ab87369e11
Stavr101 <95885361+Stavr101@users.noreply.github.com>
2022-07-28 14:13:48 +0000

    Deploying to gh-pages from @ Stavr101/goit-js-hw-09@559c53567fc99438c9a8424c4456972cefd43645 🚀

---
+++ 01-color-switcher.0ead9361.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};document.querySelector(".color");t.startBtn.addEventListener("click",onStartClick),t.stopBtn.addEventListener("click",stopClick)}();
+//# sourceMappingURL=01-color-switcher.0ead9361.js.map
